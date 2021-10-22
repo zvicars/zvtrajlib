dc9df03b9578dca93327eb5de5d9b5a490fe1afe
zvicars <vicars@seas.upenn.edu>
2021-10-22 13:03:05 -0400

    attempt2

--- analysis/calculations/Calc_2D_Density.cpp
+++ analysis/calculations/Calc_2D_Density.cpp
@@ -116,7 +116,7 @@ void Calc_2D_Density::add_gaussian(double x_in, double y_in)
         xmax = xmin + grid_spacing_[0];
         ymin = iy * grid_spacing_[1];
         ymax = ymin + grid_spacing_[1];        
-        grid_density_[gridIndex(getBin(x,0), getBin(x,1))] += h_x(x, xmin, xmax, sigma_, 2.0*sigma_)*h_x(y, ymin, ymax, sigma_, 2.0*sigma_);
+        grid_density_[gridIndex(idx, idy)] += h_x(x, xmin, xmax, sigma_, 2.0*sigma_)*h_x(y, ymin, ymax, sigma_, 2.0*sigma_);
       }
     }
     return;
