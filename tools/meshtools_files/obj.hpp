825409ef09e03719a154d440c1a7e2fe40c05d67
zvicars <vicars@seas.upenn.edu>
2022-04-17 21:43:42 -0400

    .

--- tools/meshtools_files/obj.hpp
+++ tools/meshtools_files/obj.hpp
@@ -6,14 +6,14 @@
 #include <array>
 class ObjFile : public MeshFile{
 public:
-  virtual int load(std::string file_text);
-  virtual int save( const Mesh& mesh, std::string& file_text);
+  int load(std::string file_text);
+  int save( const Mesh& mesh, std::string& file_text);
 protected:
   void createPerVertexNormals();
   void readVertex(std::string line);
   void readVertexNormal(std::string line);
   void readFace(std::string line);
-  virtual void setMesh();
+  void setMesh();
   std::vector<std::array<double,3> > vertices_;
   std::vector<int> vertex_normals_;
   std::vector<std::array<int,3> > faces_, face_vtx_norms_;
