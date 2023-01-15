162c1750f8f3408313fb235225247d0fe9e8ecfb
zvicars <vicars@seas.upenn.edu>
2023-01-15 08:49:36 -0500

    small changes

--- analysis/calculations/Calc_1D_Density_IP.cpp
+++ analysis/calculations/Calc_1D_Density_IP.cpp
@@ -92,7 +92,6 @@ void Calc_1D_Density_IP::calculate(){
   int counter = 0;
   double com_dx_ = 0.0;
   if(com_corr_ != 0){
-    double com_offset_temp = 0.0;
     //first, figure out if split near edge of box by comparing variances for 
     //normal and half-box shifted configurations
     double var0=0.0, var1=0.0, com0=0.0, com1=0.0;
@@ -111,7 +110,7 @@ void Calc_1D_Density_IP::calculate(){
     var1 /= (double)indices.size();
     //if half-length shifting gives lower variance, then we'll want to bake in an initial half-length shift
     //suggests that density is straddling box edge
-    if(var1 < var0)com_dx_ = 0.5*box_size_ - com1;
+    if(var1 < var0) com_dx_ = 0.5*box_size_ - com1;
     else com_dx_ = -com0;
     //if comm_corr_ == 1, we want the high density to be in the center of the box instead of 0
     if(com_corr_ == 1) com_dx_ += 0.5*box_size_;
@@ -136,6 +135,7 @@ void Calc_1D_Density_IP::calculate(){
 
     auto position = box->atoms[idx].x;
     position[dim_] += com_dx_;
+    position[dim_] = wrapNumber(position[dim_], box_size_);
     if(coarseGrain) add_gaussian(position[dim_]);
     else putInBin(position);
   }
