1fde8a0559ea176acbeedcddbb3ff9d582b9f3bb
zvicars <vicars@seas.upenn.edu>
2023-02-04 00:00:26 -0500

    fixed initialization error

--- analysis/probevolumes/PV_Simple.hpp
+++ analysis/probevolumes/PV_Simple.hpp
@@ -6,5 +6,5 @@ public:
   PV_DiscreteRect(std::array<double,2> xrange, std::array<double,2> yrange, std::array<double,2> zrange);
   virtual double compute(Vec3<double> position);
 private:
-  Vec<double> axis_ranges_;
+  std::array<double,6> axis_ranges_;
 };
